04373b09fda7c59b9396aa2c57f1e5bb3c08d303
Dimitri M <ibmiti@gmail.com>
2021-07-05 15:32:30 -0400

    adding protected ' custom ' primary-key

--- app/Models/Articles.php
+++ app/Models/Articles.php
@@ -10,4 +10,11 @@ class Articles extends Model
     use HasFactory;
 
     protected $table = 'articles';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'article_id';
 }
